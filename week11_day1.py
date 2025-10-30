39ad9d10b8428d432d8801caeb992f737889007c
Schmoopster <lomaxso.228@gmail.com>
2025-10-30 13:49:08 +0000

    week11_day.py

--- week11_day1.py
+++ week11_day1.py
@@ -22,17 +22,25 @@ print(first_name.upper())
 print(len(first_name))
 
 # # 3. Combine your name and favorite color into one message
-print("My name is", (first_name), "and my favorite color is", (favorite_color))
+print("My name is", (first_name), "and my favorite color is", (favorite_color.lower()))
 
 # #  Step 3: Math Practice
 # # Use arithmetic operators with your favorite number
-
-
+num1 = 3
+num2 = 3
+num3 = input("what is your 3rd fav number")
+num4 = input("what is your 4th fav number")
+print(num1 + num2)
+print(num3 + num4)
 # #  Step 4: User Input Practice
 # # Ask the user two questions and combine answers
-
-
-
+num5 = input("what condiment is yellow and comes from seeds and is put on hotdogs?")
+num6 = input("What number comes after 66?")
+print(num5 + num6)
 
 # # ⚙️ Step 5: Final Challenge (combine it all)
 # # Use math and strings together
+num7 = 66
+num8 = 1
+print("Do you know what 66 + 1 is?")
+print("Yeah, it is", (num7 + num8))
